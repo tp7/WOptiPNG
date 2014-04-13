d02b6671e6ce4df16e1c0ed9bf626944e81c3343
Victor Efimov <tp7ptr@gmail.com>
2014-04-13 22:51:04 +1100

    Format sizes in a less retarded way

---
+++ WOptiPng/Converters/SizeHumanizerConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace WOptiPng.Converters
+{
+    [ValueConversion(typeof (long), typeof (string))]
+    public class SizeHumanizerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof (string))
+            {
+                throw new InvalidOperationException("The target must be a string");
+            }
+
+            return value == null ? null : string.Format(culture, "{0:###,###,###.##} KB", ((long)value)/1024.0f);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
